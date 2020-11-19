0b22cd88f739543d178ea087da8125370fdbc4b0
Tony Blum <tonyblum@me.com>
2020-11-19 10:53:17 -0800

    heroku

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_12_122744) do
+ActiveRecord::Schema.define(version: 2020_11_12_130029) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2020_11_12_122744) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+    t.integer "movie_id"
   end
 
   create_table "users", force: :cascade do |t|
